2a871be2ee23ed1d9e282438237a4e77228b4943
unknown <nguyentrucanhtuan@gmail.com>
2016-10-31 10:22:59 +0700

    update secrect code 41

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ function sendTextMessage(sender, text) {
 		json: {
 			recipient: {id:sender},
 			message: messageData,
-			sender_action: "typing_on"
 		}
 	}, function(error, response, body) {
 		if (error) {
