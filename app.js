dad4f1e46c08b3b8328b20f894fe9589780a70c3
unknown <nguyentrucanhtuan@gmail.com>
2016-11-01 00:07:03 +0700

    add menu

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
-	menuFix();
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
@@ -145,81 +144,39 @@ function sendVideo(sender) {
 	})
 }
 
-function menuFix(sender){
-	/*
-	{
-		"setting_type" : "call_to_actions",
-		"thread_state" : "existing_thread",
-		"call_to_actions":[
-			{
-				"type":"postback",
-				"title":"Help",
-				"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
-			},
-			{
-				"type":"postback",
-				"title":"Start a New Order",
-				"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
-			},
-			{
-				"type":"web_url",
-				"title":"Checkout",
-				"url":"http://petersapparel.parseapp.com/checkout",
-				"webview_height_ratio": "full",
-				"messenger_extensions": true
-			},
-			{
-				"type":"web_url",
-				"title":"View Website",
-				"url":"http://petersapparel.parseapp.com/"
-			}
-		]
-	}*/
-
-	let messageData = {
-			"setting_type" : "call_to_actions",
-			"thread_state" : "existing_thread",
-			"call_to_actions":[
-				{
-					type:"postback",
-					title:"Help",
-					payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
-				},
-				{
-					type:"postback",
-					title:"Start a New Order",
-					payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
-				},
-				{
-					type:"web_url",
-					title:"Checkout",
-					url:"http://nguyenlieuphache.com.vn/",
-					webview_height_ratio: "full",
-					messenger_extensions: true
-				},
-				{
-					type:"web_url",
-					title:"View Website",
-					url:"http://nguyenlieuphache.com/"
-				}
-			]
-	}
-
+function addPersistentMenu(){
 	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-			
-		}
+	    url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+	    qs: {access_token:token},
+	    method: 'POST',
+	    json:{
+	        setting_type : "call_to_actions",
+	        thread_state : "existing_thread",
+	        call_to_actions:[
+	            {
+	              type:"postback",
+	              title:"Home",
+	              payload:"home"
+	            },
+	            {
+	              type:"postback",
+	              title:"Joke",
+	              payload:"joke"
+	            },
+	            {
+	              type:"web_url",
+	              title:"DMS Software Website",
+	              url:"http://www.dynamic-memory.com/"
+	            }
+	          ]
+	    }
 	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
+	    console.log(response)
+	    if (error) {
+	        console.log('Error sending messages: ', error)
+	    } else if (response.body.error) {
+	        console.log('Error: ', response.body.error)
+	    }
 	})
 }
 
