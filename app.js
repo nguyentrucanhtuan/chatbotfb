a1a4d4ce2dcb1d3dbb3b748b04f905a5431132ae
Lam Huynh <tlamhuynh@gmail.com>
2016-11-10 21:53:11 +0700

    ADD on postback listening

--- app.js
+++ app.js
@@ -23,11 +23,7 @@ mbot.setGetStartedButton("GET_STARTED");
 
 mbot.on('postback', (payload, reply, actions) => {
 	let postback = payload.postback.payload;
-	reply({ postback }, (err) => {
-      if (err) throw err
-
-      //console.log(`Echoed postback to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
+	reply({ postback })
 	
 	if (payload.postback.payload == "GET_STARTED") {
         getStarted(payload.sender.id);
