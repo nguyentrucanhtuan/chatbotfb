2242f7980a28f096305612d3e5bae9012bafc426
Lam Huynh <tlamhuynh@gmail.com>
2016-11-10 20:53:02 +0700

    ADD new Stucture bot-messenger package

--- app.js
+++ app.js
@@ -1,308 +1,54 @@
-/*var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World COffeetree!');
-});
-
-var port = process.env.PORT || 8080;
-
-app.listen(port, function () {
-  console.log('Example app listening on port!' + port);
-});*/
 'use strict'
 
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const app = express()
+const express = require('express')
+const http = require('http')
+const MBot = require('messenger-bot')
+const Config = require('./const.js');
+let mbot = new MBot({
+  token: Config.FB_PAGE_TOKEN,
+  verify: Config.FB_VERIFY_TOKEN,
+  app_secret: Config.FB_APP_SECRECT
+})
 
 
-const FB = require('./facebook.js');
+mbot.on('error', (err) => {
+  console.log(err.message)
+})
 
-app.set('port', (process.env.PORT || 5000))
+mbot.on('message', (payload, reply) => {
+  let text = payload.message.text
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
 
-// parse application/json
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log('Echoed back to ${profile.first_name} ${profile.last_name}: ${text}')
+    })
+  })
+})
+let app = express()
+
 app.use(bodyParser.json())
 
-// index
-app.get('/', function (req, res) {
-	res.send('hello world i am a secret bot')
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/webhook/', (req, res) => {
+  return mbot._verify(req, res)
 })
 
-// for facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'anhtuandeptrailaday') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
-
-// to post data
-app.post('/webhook/', function (req, res) {
-	
-	const messaging = FB.getFirstMessagingEntry(req.body);
-	if (messaging && messaging.message) {
-		const sender = messaging.sender.id;
-		const msg = messaging.message.text;
-		const atts = messaging.message.attachments;
-		console.log(msg);
-		console.log(sender);
-		if (atts) {
-		  // We received an attachment
-
-		  // Let's reply with an automatic message
-		  FB.fbMessage(
-			sender,
-			'Sorry I can only process text messages for now.'
-		  );
-		} else if (msg) {
-			/*FB.fbMessage(
-			sender,
-			'You said: '+msg
-			);*/
-			sendTextMessage(sender, "Bạn vừa nói là: " + msg);
-		}
-	}
-	
-	res.sendStatus(200);
-	/*addGreeting();
-	addPersistentMenu();
-
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = req.body.entry[0].messaging[i]
-		let sender = event.sender.id
-		if (event.message && event.message.text) {
-			let text = event.message.text
-			if (text === 'Generic') {
-				sendGenericMessage(sender)
-				continue
-			}
-			if (text === 'Image') {
-				sendImage(sender)
-				continue
-			}
-			sendTextMessage(sender, "Bạn vừa nói là: " + text.substring(0, 200))
-		}
-		if (event.postback) {
-			let text = JSON.stringify(event.postback)
-			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-			continue
-		}
-	}
-	res.sendStatus(200)*/
+app.post('/webhook/', (req, res) => {
+  mbot._handleMessage(req.body)
+  res.end(JSON.stringify({status: 'ok'}))
 })
 
 
-// recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.PAGE_ACCESS_TOKEN
-const token = "EAAHwsu50wLoBAGGRgSUg5xZCy3sHmdDW4AlkUXrZA9uZBk8IccCDbwzmBZBsmT88wB8w9ds0MXH754SnPpZCPayB5rnUw3DBBcxjEGcYiZAnifLvpH0N5DdeuO3NkdFHtzBc9qZB13s7U4YrYjm8py6obHhBu3sIU6ZBDp7rnCvk7wZDZD"
 
-function sendTextMessage(sender, text) {
-	let messageData = { text:text }
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function sendImage(sender) {
-	let messageData = {
-      attachment: {
-        type: "image",
-        payload: {
-          url: "http://nguyenlieuphache.com/catalog/view/theme/nlpc2/images/cong-thuc-soda.png"
-        }
-      }
-    }
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function sendVideo(sender) {
-	let messageData = {
-      attachment: {
-        type: "video",
-        payload: {
-          url: "https://petersapparel.com/bin/clip.mp4"
-        }
-      }
-    }
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function addPersistentMenu(){
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-		qs: {access_token:token},
-		method: 'POST',
-		json:{
-			setting_type : "call_to_actions",
-			thread_state : "existing_thread",
-			call_to_actions:[
-				{
-					type:"postback",
-					title:"Danh sách sản phẩm",
-					payload:"PRODUCT_LIST"
-				},
-				{
-					type:"postback",
-					title:"Phản hồi và hỗ trợ",
-					payload:"FEEDBACK_AND_HELP"
-				},
-				{
-					type:"postback",
-					title:"Chia sẻ",
-					payload:"SHARE"
-				}
-			]
-		}
-	}, function(error, response, body) {
-		console.log(response)
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function addGreeting(){
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-		qs: {access_token:token},
-		method: 'POST',
-		json:{
-			setting_type: "greeting",
-			greeting: {
-				text: "Xin chào {{user_first_name}} đã đến với trang cung cấp nguyên liệu pha chế TNT, Tôi sẽ giúp bạn tìm thấy thứ bạn cần và hãy bắt đầu mua sắm nào!!!"
-			}
-			
-		}
-	}, function(error, response, body) {
-		console.log(response)
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function sendAction(sender, action){
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json:{
-			recipient: {id:sender},
-			sender_action: action,
-		}
-	}, function(error, response, body) {
-		console.log(response)
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
-}
-
-// spin spin sugar
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+http.createServer(app).listen((process.env.PORT || 5000))
--- const.js
+++ const.js
@@ -11,6 +11,9 @@ if (!WIT_TOKEN) {
 //const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 const FB_PAGE_TOKEN = "EAAHwsu50wLoBAGGRgSUg5xZCy3sHmdDW4AlkUXrZA9uZBk8IccCDbwzmBZBsmT88wB8w9ds0MXH754SnPpZCPayB5rnUw3DBBcxjEGcYiZAnifLvpH0N5DdeuO3NkdFHtzBc9qZB13s7U4YrYjm8py6obHhBu3sIU6ZBDp7rnCvk7wZDZD";
 
+//const FB_APP_SECRECT = process.env.FB_APP_SECRECT;
+
+const FB_APP_SECRECT = "5a0c8ecb38855d9aba177211203764db";
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
@@ -18,8 +21,11 @@ if (!FB_VERIFY_TOKEN) {
   FB_VERIFY_TOKEN = "anhtuandeptrailaday";
 }
 
+
+
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
   FB_PAGE_TOKEN: FB_PAGE_TOKEN,
   FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
+  FB_APP_SECRECT: FB_APP_SECRECT,
 };
