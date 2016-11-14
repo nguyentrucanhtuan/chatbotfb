a5d6972b6b420c9ed861ec991ed64322e913deba
Lam Huynh <tlamhuynh@gmail.com>
2016-11-14 19:37:17 +0700

    use fb-bot-framework

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const request = require('request')
 const http = require('http')
 const MBot = require('messenger-bot')
 const Config = require('./const.js');
+
+let FBBotFramework = require('fb-bot-framework');
+/*
 let mbot = new MBot({
   token: Config.FB_PAGE_TOKEN,
   verify: Config.FB_VERIFY_TOKEN,
@@ -56,7 +59,7 @@ mbot.on('message', (payload, reply) => {
     if (err) throw err
 
 	
-	/*switch (text){
+	switch (text){
 		case 'say hi':
 		
 			break;
@@ -65,7 +68,7 @@ mbot.on('message', (payload, reply) => {
 			break;
 			
 		case ''
-	}*/
+	}
     reply({ text }, (err) => {
       if (err) throw err
 
@@ -73,23 +76,85 @@ mbot.on('message', (payload, reply) => {
     })
   })
 })
+*/
+
+var bot = new FBBotFramework({
+    page_token: Config.FB_PAGE_TOKEN,
+    verify_token: Config.FB_VERIFY_TOKEN
+});
+
+
 let app = express()
 
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
+app.use('/webhook', bot.middleware());
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+//app.use(bodyParser.urlencoded({
+//  extended: true
+//}))
 
-app.get('/webhook/', (req, res) => {
-  return mbot._verify(req, res)
-})
+//app.get('/webhook/', (req, res) => {
+//  return mbot._verify(req, res)
+//})
 
-app.post('/webhook/', (req, res) => {
-  mbot._handleMessage(req.body)
-  res.end(JSON.stringify({status: 'ok'}))
-})
+//app.post('/webhook/', (req, res) => {
+ // mbot._handleMessage(req.body)
+ // res.end(JSON.stringify({status: 'ok'}))
+//})
 
+bot.on('message', function(userId, message){
+    bot.sendTextMessage(userId, "Echo Message:" + message);
+});
+
+
+bot.setGetStartedButton("GET_STARTED");
+bot.on('postback', function(userId, payload){
+
+    if (payload == "GET_STARTED") {
+        getStarted(userId);
+    }
+
+    // Other postback callbacks here
+    // ...
+
+});
+
+
+function getStarted(userId){
+
+    // Get started process 
+}
+
+// Setup listener for attachment
+bot.on('attachment', function(userId, attachment){
+
+    // Echo the audio attachment
+    if (attachment[0].type == "audio") {
+        bot.sendAudioAttachment(userId, attachment[0].payload.url);
+    }
+
+});
+
+
+
+var menuButtons = [
+    {
+        "type": "postback",
+        "title": "Help",
+        "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+    },
+    {
+        "type": "postback",
+        "title": "Start a New Order",
+        "payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
+    },
+    {
+        "type": "web_url",
+        "title": "View Website",
+        "url": "http://nguyenlieuphache.com.vn"
+    }
+];
+bot.setPersistentMenu(menuButtons);
 
 
 http.createServer(app).listen((process.env.PORT || 5000))
