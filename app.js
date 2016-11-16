0b9a369bacab8f3b9fb2b365bb1b1eedc99e57cb
Lam Huynh <tlamhuynh@gmail.com>
2016-11-16 15:19:41 +0700

    update middleware2

--- app.js
+++ app.js
@@ -218,12 +218,14 @@ function getStartShoppingPostBack(userId){
     ];
 	console.log("enter quick replies start shopping");
 	bot.sendQuickReplies(userId,text,replies,"REGULAR", function(err,result){
+		console.log("print result quick reply");
 		console.log(result);
+		console.log("end print result quick reply");
 		//if(result.quick_reply.payload == "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_USED_SHOP"){
 			
 		//}
 		
-		//if(result.quick_reply.payload == "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_NEW_SHOP"){
+		///if(result.quick_reply.payload == "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_NEW_SHOP"){
 			
 		//}
 		
