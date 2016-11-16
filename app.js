196864a5b34dc79c813d725e100cdec076b6eadc
Lam Huynh <tlamhuynh@gmail.com>
2016-11-16 18:13:04 +0700

    update middleware2 quickreply event

--- app.js
+++ app.js
@@ -333,6 +333,11 @@ bot.on('postback', function(userId, payload){
 		getShowBartenderMaterialPostBack(userId);
 	}
 	
+	
+	if (payload == "DEVELOPER_DEFINED_PAYLOAD_FOR_BUY_COFFEE_ROBUSTA"){
+		getBuyCoffeeRobustaPostBack(userId);
+	}
+	
 
     // Other postback callbacks here
     // ...
@@ -395,7 +400,7 @@ function getShowCoffeePostBack(userId){
 				{
 					"type": "postback",
 					"title": "Mua 155.000 VNĐ/Kg",
-					"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_SHOW_COFFEE_ROBUSTA"
+					"payload": "DEVELOPER_DEFINED_PAYLOAD_FOR_BUY_COFFEE_ROBUSTA"
 				}
 			]
 		},
@@ -471,6 +476,62 @@ function getShowBartenderMaterialPostBack(userId){
 
 function getShowBartenderToolPostBack(userId){
 	
+}
+
+
+function getBuyCoffeeRobustaPostBack(userId){
+	var receipt = {
+		"recipient_name": "Stephane Crozatier",
+		"order_number": "12345678902",
+		"currency": "USD",
+		"payment_method": "Visa 2345",
+		"order_url": "http://petersapparel.parseapp.com/order?order_id=123456",
+		"timestamp": "1428444852",
+		"elements": [
+			{
+				"title": "Classic White T-Shirt",
+				"subtitle": "100% Soft and Luxurious Cotton",
+				"quantity": 2,
+				"price": 50,
+				"currency": "USD",
+				"image_url": "http://petersapparel.parseapp.com/img/whiteshirt.png"
+			},
+			{
+				"title": "Classic Gray T-Shirt",
+				"subtitle": "100% Soft and Luxurious Cotton",
+				"quantity": 1,
+				"price": 25,
+				"currency": "USD",
+				"image_url": "http://petersapparel.parseapp.com/img/grayshirt.png"
+			}
+		],
+		"address": {
+			"street_1": "1 Hacker Way",
+			"street_2": "",
+			"city": "Menlo Park",
+			"postal_code": "94025",
+			"state": "CA",
+			"country": "US"
+		},
+		"summary": {
+			"subtotal": 75.00,
+			"shipping_cost": 4.95,
+			"total_tax": 6.19,
+			"total_cost": 56.14
+		},
+		"adjustments": [
+			{
+				"name": "New Customer Discount",
+				"amount": 20
+			},
+			{
+				"name": "$10 Off Coupon",
+				"amount": 10
+			}
+		]
+	};
+
+	bot.sendReceiptMessage(recipient, receipt);
 }
 
 function getStarted(userId){
